6d08c1ba00eb55b0c8e96624613258b2d880a4d5
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-06 05:51:45 -0800

    d

--- MICRO_SERVE/main.php
+++ MICRO_SERVE/main.php
@@ -5,33 +5,32 @@ $realm = 'Restricted area';
 $users = array('sysadmin' => 'ADMINPASS', 'guest' => 'GUESTPASS');
 
 
-if (empty($_SERVER['PHP_AUTH_DIGEST']) || !($data = http_digest_parse($_SERVER['PHP_AUTH_DIGEST'])) ||  !isset($users[$data['username']]))  {
+if (empty($_SERVER['PHP_AUTH_DIGEST']) || !($data = http_digest_parse($_SERVER['PHP_AUTH_DIGEST'])) ||  !isset($users[$data['username']])) {
     header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Digest realm="'.$realm.
-           '",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
+    header('WWW-Authenticate: Digest realm="' . $realm .
+        '",qop="auth",nonce="' . uniqid() . '",opaque="' . md5($realm) . '"');
 
     die('Text to send if user hits Cancel button');
 }
 
+echo 'You are logged in as: ' . $data['username'];
 
 // generate the valid response
 $A1 = md5($data['username'] . ':' . $realm . ':' . $users[$data['username']]);
-$A2 = md5($_SERVER['REQUEST_METHOD'].':'.$data['uri']);
-$valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.$A2);
+$A2 = md5($_SERVER['REQUEST_METHOD'] . ':' . $data['uri']);
+$valid_response = md5($A1 . ':' . $data['nonce'] . ':' . $data['nc'] . ':' . $data['cnonce'] . ':' . $data['qop'] . ':' . $A2);
 
 if ($data['response'] != $valid_response)
-  echo('Wrong Credentials!');
-  header("Refresh:0");
-
+    die("Wrong Credentials!");
 // ok, valid username & password
-echo 'You are logged in as: ' . $data['username'];
+
 
 
 // function to parse the http auth header
 function http_digest_parse($txt)
 {
     // protect against missing data
-    $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
+    $needed_parts = array('nonce' => 1, 'nc' => 1, 'cnonce' => 1, 'qop' => 1, 'username' => 1, 'uri' => 1, 'response' => 1);
     $data = array();
     $keys = implode('|', array_keys($needed_parts));
 
@@ -58,49 +57,45 @@ $password = "REPLACE_WITH_YOUR_PASSWORD";
 // If you change this value, the ESP32 sketch needs to match
 $api_key_value = "tPmAT5Ab3j7F9";
 
-$api_key= $sensor = $location = $value1 = $value2 = $value3 = "";
+$api_key = $sensor = $location = $value1 = $value2 = $value3 = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
-    if($api_key == $api_key_value) {
+    if ($api_key == $api_key_value) {
         $sensor = test_input($_POST["sensor"]);
         $location = test_input($_POST["location"]);
         $value1 = test_input($_POST["value1"]);
         $value2 = test_input($_POST["value2"]);
         $value3 = test_input($_POST["value3"]);
-        
+
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
-        } 
-        
+        }
+
         $sql = "INSERT INTO SensorData (sensor, location, value1, value2, value3)
         VALUES ('" . $sensor . "', '" . $location . "', '" . $value1 . "', '" . $value2 . "', '" . $value3 . "')";
-        
+
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
-        } 
-        else {
+        } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
-    
+
         $conn->close();
-    }
-    else {
+    } else {
         echo "Wrong API Key provided.";
     }
-
-}
-else {
+} else {
     echo "No data posted with HTTP POST.";
 }
 
-function test_input($data) {
+function test_input($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
-
