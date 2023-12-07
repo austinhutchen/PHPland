dcd8ac845dbecbbaac75ae139d70ed43ba332fbe
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-07 05:46:13 -0800

    d

--- MICRO_SERVE/main.php
+++ MICRO_SERVE/main.php
@@ -21,49 +21,6 @@ $valid_response = md5($A1 . ':' . $data['nonce'] . ':' . $data['nc'] . ':' . $da
 
 
 
-// Function to send GET request to the ESP32 server
-function sendGetRequest($url) {
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-    ));
-
-    $response = curl_exec($curl);
-    curl_close($curl);
-
-    return $response;
-}
-
-// Function to send POST request to the ESP32 server
-function sendPostRequest($url, $data) {
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => $data,
-    ));
-
-    $response = curl_exec($curl);
-    curl_close($curl);
-
-    return $response;
-}
-
-// Example GET request to the ESP32
-$esp32_ip = 'ESP32_SERVER_IP_ADDRESS';
-$get_response = sendGetRequest("http://$esp32_ip/some_endpoint");
-
-echo "GET Response: " . $get_response . "\n";
-
 // Example POST request to the ESP32
 $post_data = array(
     'param1' => 'value1',
